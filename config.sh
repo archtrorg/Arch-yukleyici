6f10fafe46e632ee5e772a057deb890cf94c8d64
archkur <archtrorg@gmail.com>
2018-04-10 23:01:13 +0300

    yeni dosya

---
+++ config.sh
@@ -0,0 +1,294 @@
+#!/bin/bash
+
+set -e
+
+color(){
+    case $1 in
+        red)
+            echo -e "\033[31m$2\033[0m"
+        ;;
+        yellow)
+            echo -e "\033[33m$2\033[0m"
+        ;;
+    esac
+}
+
+config_base(){
+    color yellow "Bilgisayar adını girin"
+    read TMP
+    echo $TMP > /etc/hostname
+    color yellow "ROOT parolanızı girin"
+    passwd
+}
+
+config_locale(){
+    color yellow "Lütfen yerel saatinizi seçin"
+    select TIME in `ls /usr/share/zoneinfo`;do
+        if [ -d "/usr/share/zoneinfo/$TIME" ];then
+            select time in `ls /usr/share/zoneinfo/$TIME`;do
+                ln -sf /usr/share/zoneinfo/$TIME/$time /etc/localtime
+                break
+            done
+        else
+            ln -sf /usr/share/zoneinfo/$TIME /etc/localtime
+            break
+        fi
+        break
+    done
+    hwclock --systohc --utc
+    color yellow "Dilinizi seçin"
+    select LNAG in "tr_TR.UTF-8" "en_US.UTF-8";do
+        echo "$LNAG UTF-8" > /etc/locale.gen
+        locale-gen
+        echo LANG=$LANG > /etc/locale.conf
+        break
+    done
+}
+
+install_grub(){
+    if (mount | grep efivarfs > /dev/null 2>&1);then
+        pacman -S --noconfirm grub efibootmgr -y
+        grub-install --target=`uname -m`-efi --efi-directory=/boot --bootloader-id=Arch
+        grub-mkconfig -o /boot/grub/grub.cfg
+    else
+        pacman -S --noconfirm grub
+        fdisk -l
+        color yellow "Input the disk you want to install grub (/dev/sdX"
+        read TMP
+        grub-install --target=i386-pc $TMP
+        grub-mkconfig -o /boot/grub/grub.cfg
+    fi
+}
+
+install_bootctl(){
+    if (mount | grep efivarfs > /dev/null 2>&1);then
+        bootctl --path=esp install
+        cp /usr/share/systemd/bootctl/loader.conf /boot/loader/
+	echo "timeout 4" >> /boot/loader/loader.conf
+	echo -e "title          Arch Linux\nlinux          /vmlinuz-linux\ninitrd         /initramfs-linux.img" > /boot/loader/entries/arch.conf
+
+    else
+        color yellow "Looks like your PC doesn't suppot UEFI or not in UEFI mode ENTER to use grub. Input q to quit"
+        read TMP
+        if [ "$TMP" == "" ];then
+            install_grub
+        else
+            exit
+        fi
+    fi
+}
+
+add_user(){
+    color yellow "Kullanmak istediğiniz kullanıcı adını girin (must be lower case)"
+    read USER
+    useradd -m -g wheel $USER
+    color yellow "Parolanızı girin"
+    passwd $USER
+    pacman -S --noconfirm sudo
+    sed -i 's/\# \%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/g' /etc/sudoers
+    sed -i 's/\# \%wheel ALL=(ALL) NOPASSWD: ALL/\%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
+}
+
+install_graphic(){
+    color yellow "Hangi video sürücüsü kurulsun?"
+    select GPU in "Intel" "Nvidia" "Intel and Nvidia" "AMD";do
+        case $GPU in
+            "Intel")
+                pacman -S --noconfirm xf86-video-intel -y
+                break
+            ;;
+            "Nvidia")
+                color yellow "Hangi versiyon yüklensin"
+                select NVIDIA in "GeForce-8 and newer" "GeForce-6/7" "Older";do
+                    case $NVIDIA in
+                        "GeForce-8 and newer")
+                            pacman -S --noconfirm nvidia -y
+                            break
+                        ;;
+                        "GeForce-6/7")
+                            pacman -S --noconfirm nvidia-304xx -y
+                            break
+                        ;;
+                        "Older")
+                            pacman -S --noconfirm nvidia-340xx -y
+                            break
+                        ;;
+                        *)
+                            color red "Error ! Please input the correct num"
+                        ;;
+                    esac
+                done
+                break
+            ;;
+            "Intel and Nvidia")
+                pacman -S --noconfirm bumblebee -y
+                systemctl enable bumblebeed
+                color yellow "Version of nvidia-driver to install"
+                select NVIDIA in "GeForce-8 and newer" "GeForce-6/7" "Older";do
+                    case $NVIDIA in
+                        "GeForce-8 and newer")
+                            pacman -S --noconfirm nvidia -y
+                            break
+                        ;;
+                        "GeForce-6/7")
+                            pacman -S --noconfirm nvidia-304xx -y
+                            break
+                        ;;
+                        "Older")
+                            pacman -S --noconfirm nvidia-340xx -y
+                            break
+                        ;;
+                        *)
+                            color red "Error ! Please input the correct num"
+                        ;;
+                    esac
+                done
+                break
+            ;;
+            "AMD")
+                pacman -S --noconfirm xf86-video-ati -y
+                break
+            ;;
+            *)
+                color red "Hata ! Lütfen doğru numarayı giriniz"
+            ;;
+        esac
+    done
+}
+
+install_bluetooth(){
+    pacman -S --noconfirm bluez
+    systemctl enable bluetooth
+    color yellow "Blueman yüklensin mi? y)YES ENTER)NO"
+    read TMP
+    if [ "$TMP" == "y" ];then
+        pacman -S --noconfirm blueman
+    fi
+}
+
+install_app(){
+    color yellow "Yaourt yüklensin mi? (yalnız çin kullanıcıları için) y)YES ENTER)NO"
+    read TMP
+    if [ "$TMP" == "y" ];then
+        sed -i '/archlinuxcn/d' /etc/pacman.conf
+        sed -i '/archlinux-cn/d' /etc/pacman.conf
+        select MIRROR in "USTC" "TUNA" "163";do
+            case $MIRROR in
+                "USTC")
+                    echo -e "[archlinuxcn]\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+                    break
+                ;;
+                "TUNA")
+                    echo -e "[archlinuxcn]\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+                    break
+                ;;
+                "163")
+                    echo -e "[archlinuxcn]\nServer = http://mirrors.163.com/archlinux-cn/\$arch" >> /etc/pacman.conf
+                    break
+                ;;
+                *)
+                    color red "Hata ! Lütfen doğru numarayı giriniz"
+                ;;
+            esac
+        done
+        pacman -Sy
+        pacman -S --noconfirm archlinuxcn-keyring
+        pacman -S --noconfirm yaourt
+    else
+        pacman -S --noconfirm git
+        su - $USER -c "cd ~
+            git clone https://aur.archlinux.org/package-query.git
+            cd package-query&&makepkg -si
+            cd ..
+            git clone https://aur.archlinux.org/yaourt.git
+            cd yaourt&&makepkg -si
+            cd ..
+            rm -rf package-query yaourt"
+        fi
+    pacman -S --noconfirm networkmanager xorg-server firefox wqy-zenhei
+    systemctl enable NetworkManager
+    if [ "$GPU" == "Intel and Nvidia" ];then
+        gpasswd -a $USER bumblebee
+    fi
+}
+
+install_desktop(){
+    color yellow "Kullanmak istediğiniz masaüstünü seçin"
+    select DESKTOP in "KDE" "Gnome" "Lxde" "Lxqt" "Mate" "Xfce" "Deepin" "Budgie" "Cinnamon";do
+        case $DESKTOP in
+            "KDE")
+                pacman -S plasma kdebase kdeutils kdegraphics kde-l10n-zh_cn sddm
+                systemctl enable sddm
+                break
+            ;;
+            "Gnome")
+                pacman -S gnome gnome-terminal
+                systemctl enable gdm
+                break
+            ;;
+            "Lxde")
+                pacman -S lxde lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            "Lxqt")
+                pacman -S lxqt lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            "Mate")
+                pacman -S mate mate-extra mate-terminal lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            "Xfce")
+                pacman -S xfce4 xfce4-goodies xfce4-terminal lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            "Deepin")
+                pacman -S deepin deepin-extra deepin-terminal lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                sed -i '108s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-deepin-greeter/' /etc/lightdm/lightdm.conf
+                break
+            ;;
+            "Budgie")
+                pacman -S budgie-desktop gnome-terminal lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            "Cinnamon")
+                pacman -S cinnamon gnome-terminal lightdm lightdm-gtk-greeter
+                systemctl enable lightdm
+                break
+            ;;
+            *)
+                color red "Hata ! Lütfen doğru numarayı giriniz"
+            ;;
+        esac
+    done
+}
+
+main(){
+    config_base
+    config_locale
+    color yellow "GRUB veya Bootctl tercihiniz ? y)Bootctl ENTER)GRUB"
+    read TMP
+    if [ "$TMP" == "y" ];then
+        install_bootctl
+    else
+        install_grub
+    fi
+    add_user
+    install_graphic
+    color yellow "Bluetooth var mı ? y)YES ENTER)NO"
+    read TMP
+    if [ "$TMP" == "y" ];then
+        install_bluetooth
+    fi
+    install_app
+    install_desktop
+    color yellow "Bitti, kullandığınız için teşekkürler"
+}
+
+main
---
+++ live.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# 该死的颜色
+color(){
+    case $1 in
+        red)
+            echo -e "\033[31m$2\033[0m"
+        ;;
+        green)
+            echo -e "\033[32m$2\033[0m"
+        ;;
+    esac
+}
+
+partition(){
+    if (echo $1 | grep '/' > /dev/null 2>&1);then
+        other=$1
+    else
+        other=/$1
+    fi
+
+    fdisk -l
+    color green "Disk bölümünü girin (/dev/sdaX"
+    read OTHER
+    color green "Biçimlendirmek istiyormusunuz ? y)yes ENTER)no"
+    read tmp
+
+    if [ "$tmp" == y ];then
+        umount $OTHER > /dev/null 2>&1
+        color green "Biçimlendirmek için dosya sisteminin türünü belirtin"
+        select type in 'ext2' "ext3" "ext4" "btrfs" "xfs" "jfs" "fat" "swap";do
+            case $type in
+                "ext2")
+                    mkfs.ext2 $OTHER
+                    break
+                ;;
+                "ext3")
+                    mkfs.ext3 $OTHER
+                    break
+                ;;
+                "ext4")
+                    mkfs.ext4 $OTHER
+                    break
+                ;;
+                "btrfs")
+                    mkfs.btrfs $OTHER -f
+                    break
+                ;;
+                "xfs")
+                    mkfs.xfs $OTHER -f
+                    break
+                ;;
+                "jfs")
+                    mkfs.jfs $OTHER
+                    break
+                ;;
+                "fat")
+                    mkfs.fat -F32 $OTHER
+                    break
+                ;;
+                "swap")
+                    swapoff $OTHER > /dev/null 2>&1
+                    mkswap $OTHER -f
+                    break
+                ;;
+                *)
+                    color red "Hata ! Lütfen numarayı tekrar girin"
+                ;;
+            esac
+        done
+    fi
+
+    if [ "$other" == "/swap" ];then
+        swapon $OTHER
+    else
+        umount $OTHER > /dev/null 2>&1
+        mkdir /mnt$other
+        mount $OTHER /mnt$other
+    fi
+}
+
+prepare(){
+    fdisk -l
+    color green "Bölümü ayarlamak istiyor musunuz? ? y)yes ENTER)no"
+    read tmp
+    if [ "$tmp" == y ];then
+        color green "Diski girin (/dev/sdX"
+        read TMP
+        cfdisk $TMP
+    fi
+    color green "ROOT bölümünü girin(/) mount point:"
+    read ROOT
+    color green "Format atılsın mı ? y)yes ENTER)no"
+    read tmp
+    if [ "$tmp" == y ];then
+        umount $ROOT > /dev/null 2>&1
+        color green "Biçimlendirmek için dosya sisteminin türünü belirtin"
+        select type in "ext4" "btrfs" "xfs" "jfs";do
+            umount $ROOT > /dev/null 2>&1
+            if [ "$type" == "btrfs" ];then
+                mkfs.$type $ROOT -f
+            elif [ "$type" == "xfs" ];then
+                mkfs.$type $ROOT -f
+            else
+                mkfs.$type $ROOT
+            fi
+            break
+        done
+    fi
+    mount $ROOT /mnt
+    color green "Başka bir bağlama noktanız var mı ? if so please input it, such as : /boot /home and swap or just ENTER to skip"
+    read other
+    while [ "$other" != '' ];do
+        partition $other
+        color green "Hala başka bir bağlama noktası varmı ? input it or just ENTER"
+        read other
+    done
+}
+
+install(){
+
+    color green "Lütfen ülkenizi seçiniz(for Generate the pacman mirror list"
+    select COUNTRY in "AU" "AT" "BD" "BY" "BE" "BA" "BR" "BG" "CA" "CL" "CN" "CO" "HR" "CZ" "DK" "EC" "FI" "FR" "DE" "GR" "HK" "HU" "IS" "IN" "ID" "IR" "IE" "IL" "IT" "JP" "KZ" "LV" "LT" "LU" "MK" "MX" "AN" "NC" "NZ" "NO" "PH" "PL" "PT" "QA" "RO" "RU" "RS" "SG" "SK" "SI" "ZA" "KR" "ES" "SE" "CH" "TW" "TH" "TR" "UA" "GB" "US" "VN";do
+        mv /etc/pacman.d/mirrorlist /etc/mirrorlist.bak
+        color green "Yansı listesi yazılıyor , Lütfen bekleyin"
+        wget https://www.archlinux.org/mirrorlist/\?country=$COUNTRY -O /etc/pacman.d/mirrorlist.new
+        sed -i 's/#Server/Server/g' /etc/pacman.d/mirrorlist.new
+        rankmirrors -n 3 /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist
+        chmod +r /etc/pacman.d/mirrorlist
+	break
+    done
+
+    pacstrap /mnt base base-devel --force
+    genfstab -U -p /mnt > /mnt/etc/fstab
+}
+
+config(){
+    wget https://raw.githubusercontent.com/archtrorg/Arch-yükleyici/master/config.sh -O /mnt/root/config.sh
+    chmod +x /mnt/root/config.sh
+    arch-chroot /mnt /root/config.sh
+}
+
+if [ "$1" != '' ];then
+    case $1 in
+        "--prepare")
+            prepare
+        ;;
+        "--install")
+            install
+        ;;
+        "--chroot")
+            config
+        ;;
+        "--help")
+            color red "--prepare :  prepare disk and partition\n--install :  install the base system\n--chroot :  chroot into the system to install other software"
+        ;;
+        *)
+            color red "Error !\n--prepare :  prepare disk and partition\n--install :  install the base system\n--chroot :  chroot into the system to install other software"
+        ;;
+    esac
+else
+    prepare
+    install
+    config
+fi
